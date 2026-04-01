ebc7a1dbb85052067f119ce00fb8a3a22c5a87a6
Anik Chawdhury <157480805+ovichawdhuryit@users.noreply.github.com>
2026-04-02 04:37:52 +0600

    Add files via upload

---
+++ for_loop.js
@@ -0,0 +1,14 @@
+// for (var i= 0; i<100; i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+var sum=0; 
+for (var i=0; i<20; i--){
+    
+    sum= sum +i 
+
+    console.log(sum)
+}
